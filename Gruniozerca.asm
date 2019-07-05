521d7d4217980752fba87c1b9ddd834ffee44657
DrIll <GBydelek95@gmail.com>
2019-07-06 01:43:42 +0200

    Added Grunio's graphic and movement

--- Gruniozerca.asm
+++ Gruniozerca.asm
@@ -15,6 +15,7 @@ st:
 inicialization:
 	ld	a, %11100100	;Pallete registering
 	ld	[rBGP], a
+	ld	[rOBP0], a
 
 ;Setting coordinates for the viewport
 
@@ -28,7 +29,7 @@ inicialization:
 	
 	ld	hl, Sprite_Data
 	ld	de, _VRAM
-	ld	bc, 16*2		; 16 * number of sprites to load
+	ld	bc, 16*8		; 16 * number of sprites to load
 	
 .load_graphics:
 	ld	a, [hl]
@@ -81,10 +82,26 @@ inicialization:
 	ld	a, b
 	jp	nz, .clean_OAM
 
+; Preparing Grunio's personal space
+
+	ld	hl, $C100
+	ld	de, Grunio_OAM
+	ld	b, 4*6	; 4 bytes * 6 Grunio sprites
+.next_OAM_Byte:
+	ld	a, [de]
+	ld	[hl], a
+	dec	b
+	inc	hl
+	inc	de
+	ld	a, b
+	jp nz, .next_OAM_Byte
+
 ; Main loop
 
 main_loop:
 	call	wait_for_VBlank
+	call	draw_Grunio
+	call	handle_input
 .fin_vB
 	jr	main_loop
 	
@@ -92,6 +109,111 @@ wait_for_VBlank:
 	ldh	a, [rLY]
 	cp	145
 	jr	nz, wait_for_VBlank
+	ret
+
+; Subroutine drawing Grunio
+; So far no DMA, so the drawing occurs during the VBlank
+; So this is basically my own "DMA"
+
+draw_Grunio:
+	ld	hl, $FE00
+	ld	de, $C100
+	ld	b, 4*6
+.next_Grunio_byte:
+	ld	a, [de]
+	ld	[hl], a
+	inc	hl
+	inc	de
+	dec	b
+	jp	nz, .next_Grunio_byte
+	ret
+
+; Handling input and moving Grunio
+
+handle_input:
+	ld	a, %11101101
+	ld	[rP1], a
+	ld	a, [rP1]	; Reading several times
+	ld	a, [rP1]	; To secure the correct reading
+	ld	a, [rP1]
+	ld	a, [rP1]
+	cp	%11101101	; Is "left" direction pressed?
+	jp	nz, .jump_to_right
+	ld	a, [$C101]	; Grunio's horizontal position
+	cp	6			; Grunio's position can't be < 8
+	jp	z, .jump_to_right	; We want to see entire Grunio on the screen
+	ld	b, 6		; Moving all 6 sprites
+	ld	hl, $C101
+	ld	de, 2		; The offset between X positions in our "OAM"
+.move_Grunio_left:
+	ld	a, [hl]
+	sub	2			; Move sprite 2px left
+	ld	[hl], a
+	inc	hl
+	inc	hl
+	set	5, [hl]
+	add	hl, de
+	dec	b
+	ld	a, b
+	cp	0
+	jp	nz, .move_Grunio_left
+	ld	b, 3		; Swapping Grunio's sprites to the left side
+	ld	c, 4
+	ld	de, 4
+	ld	hl, $C102
+	ld	a, [hl]
+	cp	$06			; If Grunio is turned, don't turn him again
+	jp	z, .jump_to_right
+.swap_sprites_to_left:
+	ld	a, [hl]
+	add	c
+	ld	[hl], a
+	add	hl, de
+	ld	a, [hl]
+	add	c
+	ld	[hl], a
+	add	hl, de
+	ld	a, c
+	sub	4
+	ld	c, a
+	dec	b
+	ld	a, b
+	cp	0
+	jp nz, .swap_sprites_to_left
+.jump_to_right:
+	cp	%11101110	; Is "right" direction pressed?
+	jp	nz, .return
+	ld	a, [$C101]
+	cp	$92			; Grunio again can't leave the screen
+	jp	z, .return
+	ld	hl, $C101
+	ld	de, 4		; The offset between X positions in our "OAM"
+.move_Grunio_right:
+	ld	a, [hl]
+	add	2
+	ld	[hl], a
+	add	hl, de
+	dec	b
+	ld	a, b
+	cp	0
+	jp	nz, .move_Grunio_right
+	ld	b, 2		; Swapping Grunio's sprites to the right side
+	ld	de, 3
+	ld	hl, $C102
+	ld	a, [hl]
+	cp	$02			; If Grunio is turned to the right, don't turn him again
+	jp	z, .return
+.swap_sprites_to_right:
+	ld	a, b
+	ld	[hl], a
+	inc	hl
+	res	5, [hl]
+	add	hl, de
+	inc	b
+	ld	a, b
+	cp	$08
+	jp nz, .swap_sprites_to_right	
+.return:
 	ret
 
 turn_off_LCD:
@@ -104,7 +226,26 @@ turn_off_LCD:
 	ret
 	
 Sprite_Data:
-db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-db $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+db	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+db	$FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+DB	$00,$00,$0F,$0F,$1F,$18,$3F,$30	; Grunio sprites start!
+DB	$FF,$E0,$FF,$80,$FF,$80,$FF,$80
+DB $BF,$C0,$9F,$E0,$8F,$F3,$81,$FF
+DB $41,$7F,$27,$3F,$38,$38,$1E,$1E
+DB $00,$00,$C4,$C4,$FE,$3E,$FD,$07
+DB $FD,$06,$FF,$00,$FF,$00,$FF,$00
+DB $FF,$00,$FF,$00,$FF,$00,$00,$FF
+DB $00,$FF,$FF,$FF,$0E,$0E,$07,$07
+DB $00,$00,$00,$00,$00,$00,$80,$80
+DB $E0,$60,$F0,$10,$58,$28,$48,$38
+DB $C8,$38,$98,$78,$20,$E0,$40,$C0
+DB $80,$80,$00,$00,$00,$00,$00,$00	; Grunio end
+Grunio_OAM:
+db	$78, $50, $2, 0
+db	$80, $50, $3, 0
+db	$78, $58, $4, 0
+db	$80, $58, $5, 0
+db	$78, $60, $6, 0
+db	$80, $60, $7, 0
 
 EndTileCara:
